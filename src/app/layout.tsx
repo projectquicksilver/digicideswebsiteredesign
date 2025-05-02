ff28b747c0686777aa9b6da264c05a25008bcf24
HarshilTomar20 <harshiltomar20@gmail.com>
2025-05-02 14:46:10 +0530

    feat: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Digicides",
-  description: "A product-driven company dedicated exclusively to the agri industry. Our suite of advanced SaaS products and solutions helps businesses enhance marketing, communication, and customer engagement—ultimately leading to better sales and business growth.",
+  title: "SaaS Company for Agri Marketing | Digicides Marketing Services",
+  description: "Digicides is a SaaS-powered communication platform offering agriculture-focused marketing services, lead generation tools, and multi-channel engagement for agribusinesses in India.",
+  keywords: "agri marketing, SaaS for agriculture, agriculture marketing platform, farmer engagement, agri lead generation, digital marketing for agribusiness, agri SaaS solutions, multi-channel marketing agriculture, Digicides",
   icons: [{ rel: "icon", url: "/favicon.svg" }],
 };
 
